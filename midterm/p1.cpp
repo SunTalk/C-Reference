f5c47603f548354d89e7a7cfe4430f8a47a87df6
SunTalk <infernal56727@gmail.com>
2019-05-01 15:33:09 +0800

    update midterm code

--- midterm/p1.cpp
+++ midterm/p1.cpp
@@ -5,21 +5,16 @@ int main(int argc, char const *argv[])
 {	
 	string str,check;
 	while( cin >> str >> check ){
-		
 		transform(str.begin(),str.end(),str.begin(),::toupper);
 		transform(check.begin(),check.end(),check.begin(),::toupper);
 		printf("%d %d %d %d\n",count(str.begin(), str.end(),'A'),count(str.begin(), str.end(),'C'),count(str.begin(), str.end(),'G'),count(str.begin(), str.end(),'T') );
-
-		int ans = 0;
-		int tmp = str.find(check,0);
+		int ans = 0,tmp = str.find(check,0);
 		while( tmp != string::npos ){
 			printf("%d ",tmp );
 			tmp = str.find(check,tmp+1);
 			ans++;
 		}
 		printf("%d\n",ans );
-
 	}
-
 	return 0;
 }
--- midterm/p2.cpp
+++ midterm/p2.cpp
@@ -7,31 +7,24 @@ int main(int argc, char const *argv[])
 	scanf("%d %d\n",&page,&num);
 	string str;
 	cin >> str;
-	int table[10];
-	for(int i = 0 ; i < 10 ; i++ )
-		table[i] = count(str.begin(), str.end(),i+'0');
-
 	int ans,wh_page,wh_num,pa;
 	string check = "";
 	while( ~scanf("%d %d %d",&wh_page,&wh_num,&pa) ){
 		check = check + str[(wh_page-1)*num+(wh_num-1)];
 		ans = str[(wh_page-1)*num+(wh_num-1)] - '0';
-		printf("%d %d %d\n",ans ,table[ans] ,table[ans]%pa );
+		printf("%d %d %d\n",ans ,count(str.begin(), str.end(),ans+'0') ,count(str.begin(), str.end(),ans+'0')%pa );
 	}
 	cout << check ;
-
 	ans = 0;
 	string remember = check;
 	do{
 		int tmp = str.find(check,0);
-		while( tmp != -1 ){
+		while( tmp != string::npos ){
 			ans++;
 			tmp = str.find(check,tmp+1);
 		}
 		next_permutation(check.begin(), check.end());
-
 	}while( check != remember );
 	printf(" %d\n",ans );
-
 	return 0;
 }
