a902cc8060b09a7cddecb97b96b5afba4a11206c
ICEJJ <lpc0503@gmail.com>
2019-05-13 21:30:01 +0800

    add RuntimeCMP ans auto

--- HW03/HW03.cpp
+++ HW03/HW03.cpp
@@ -1,6 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class RuntimeCMP{
+
+public:
+
+	static const bool CMP_GREAT = true;
+	static const bool NORMAL   = false;
+
+	RuntimeCMP(){
+
+		mode = NORMAL;
+	}
+
+	RuntimeCMP(bool mode){
+
+		this->mode = mode;
+	}
+	bool operator()(const pair<string,int>& lhs, const pair<string,int>& rhs){
+
+		if(mode == NORMAL){
+
+			return lhs.second < rhs.second;
+		}
+		else{
+
+			return lhs.second > rhs.second;
+		}
+	}
+
+
+private:
+	bool mode;
+
+};
+
 struct CmpByValue {
   bool operator()(const pair<string,int>& lhs, const pair<string,int>& rhs) {
     return lhs.second > rhs.second;
@@ -29,15 +63,26 @@ int main(int argc, char const *argv[])
 	multimap<string,string>::iterator multi_i;
 
 	map <string,int> table;
-	for(multi_i = data.begin(); multi_i != data.end() ; multi_i++ ){
-		table.insert( make_pair((*multi_i).first,0) );
-		if( (*multi_i).second.find(payload,0) != string::npos ){
-			table.find( (*multi_i).first )->second++;
+
+	for(auto &multi_i : data){
+
+		table.insert(make_pair((multi_i).first, 0));
+		if((multi_i).second.find(payload, 0) != string::npos){
+
+			table.find((multi_i).first)->second++;
 		}
-	}
+	}	
+
+
+	// for(multi_i = data.begin(); multi_i != data.end() ; multi_i++ ){
+	// 	table.insert( make_pair((*multi_i).first,0) );
+	// 	if( (*multi_i).second.find(payload,0) != string::npos ){
+	// 		table.find( (*multi_i).first )->second++;
+	// 	}
+	// }
 
 	vector < pair<string,int> > V(table.begin(), table.end());
-	sort(V.begin(), V.end(),CmpByValue());
+	sort(V.begin(), V.end(), RuntimeCMP(RuntimeCMP::CMP_GREAT));
 
 	for(int i = 0 ; i < V.size() ; i++ ){
 		if( V[i].second > 0 )
